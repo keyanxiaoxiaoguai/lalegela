c96512e9003236509a1be480ee9be0fff67e6d4a
小亮（学习生涯） <84776779+keyanxiaoxiaoguai@users.noreply.github.com>
2022-12-03 17:50:57 +0800

    Add files via upload

---
+++ src/components/MyAd.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="my-ad">
+    <a href="https://github.com/liyupi/yulegeyu" target="_blank">
+      <div style="background: rgba(0, 0, 0, 0.8); padding: 12px">
+        <github-outlined />
+        保护环境，人人有责 
+      </div>
+    </a>
+    <!-- <a href="https://space.bilibili.com/12890453/"> D02014065 张欣羽 </a>-->
+  </div>
+</template>
+<script setup lang="ts">
+import { GithubOutlined } from "@ant-design/icons-vue";
+</script>
+<style></style>
